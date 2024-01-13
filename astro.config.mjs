a25f15c4a51ae6abeabe70a0cb46f6c53ad5788e
eloy6lega <eloy.perez@campusfp.es>
2024-01-13 13:36:25 +0100

    EditadoSinLicencia

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,14 @@
-import { defineConfig } from "astro/config";
-import sitemap from "@astrojs/sitemap";
 import netlify from "@astrojs/netlify/functions";
-import robotsTxt from "astro-robots-txt";
+import sitemap from "@astrojs/sitemap";
 import UnoCSS from "@unocss/astro";
+import { defineConfig } from "astro/config";
 
 import solidJs from "@astrojs/solid-js";
 import { remarkReadingTime } from "./src/lib/ remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://gianmarco.xyz/",
-  integrations: [
-    sitemap(),
-    robotsTxt({
-      sitemap: [
-        "https://gianmarco.xyz/sitemap-index.xml",
-        "https://gianmarco.xyz/sitemap-0.xml",
-      ],
-    }),
-    solidJs(),
-    UnoCSS({ injectReset: true }),
-  ],
+  integrations: [sitemap(), solidJs(), UnoCSS({ injectReset: true })],
   markdown: {
     remarkPlugins: [remarkReadingTime],
   },
